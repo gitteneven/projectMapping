be7f26288a8c48876f3cacd6054c9d97b1938a71
Gitte <gitte.neven@student.howest.be>
2022-10-26 11:05:33 +0200

    phaser primal state logic

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,5 +1,22 @@
+import 'phaser';
+import Scene from './Scene';
+
+const config = {
+  type: Phaser.AUTO,
+  backgroundColor: '#0827F5',
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: 'arcade',
+  },
+  scene: Scene
+};
+
 const init = () => {
-  console.log('Hello from main.ts')
+  console.log('Hello from main.ts');
+  const game = new Phaser.Game(config);
+
+
 }
 
 init()
