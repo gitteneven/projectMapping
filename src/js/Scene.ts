c1298848e578a3e5d47dd2bacf55d524c4d2ed6c
Xander Van der Sijpt <xander.vandersijpt1@gmail.com>
2022-10-26 16:28:16 +0200

    added assets and error functionality with arduino

--- src/js/Scene.ts
+++ src/js/Scene.ts
@@ -1,5 +1,7 @@
 import Phaser from "phaser";
 import HealthBar from "./HealthBar";
+// import PowerupBar from "./PowerupBar";
+import ErrorPopups from "./ErrorPopups";
 
 var text1;
 var text2;
@@ -7,19 +9,38 @@ var text3;
 var space;
 var backspace;
 let coderona;
-let user; 
+let user;
+let userEnabled = true;
+let errorCount = 0;
 
+let mouseUp, mouseLeft, mouseDown, mouseRight, powerupActivate;
+let mouseCoordinates = { x: 1040, y: 830 };
+
+let errorContainer = {
+    xStart: 226,
+    xEnd: 1864,
+    yStart: 692,
+    yEnd: 966
+}
+let errorWidth = 240;
+let errorHeight = 153;
+
+function playerHitPopup (popup, popupX) {
+    console.log('user hit popup');
+    popup.destroy();
+    popupX.destroy();
+
+    userEnabled = true;
+    errorCount -= 1;
+}
 
 export default class Scene extends Phaser.Scene {
     private player?: Phaser.Physics.Arcade.Sprite;
-    private cursors?: Phaser.Types.Input.Keyboard.CursorKeys
-
+    private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
 
     constructor() {
         super('demo');
     }
-
-
 
     preload() {
         //cursor image
@@ -34,6 +55,9 @@ export default class Scene extends Phaser.Scene {
         this.load.image('middle-shadow', 'assets/barHorizontal_shadow_mid.png')
         this.load.image('right-cap-shadow', 'assets/barHorizontal_shadow_right.png')
 
+        this.load.image('error-popup', 'assets/errorframered.png');
+        this.load.image('error-x', 'assets/errorframex.png');
+
         // this.load.image('logo', 'assets/phaser3-logo.png');
         // this.load.image('libs', 'assets/libs.png');
         // this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
@@ -43,11 +67,16 @@ export default class Scene extends Phaser.Scene {
     create() {
 
         // this.add.image(0, 0, 'cursor').setOrigin(0);
-        this.player = this.physics.add.sprite(100, 450, 'cursor')
+        this.player = this.physics.add.sprite(mouseCoordinates.x, mouseCoordinates.y, 'cursor').setOrigin(0, 0).setScale(.1).setDepth(1);
         this.cursors = this.input.keyboard.createCursorKeys()
         this.input.mouse.disableContextMenu();
         coderona = this.input.keyboard.addKey('C');
-        user = this.input.keyboard.addKey('U')
+        user = this.input.keyboard.addKey('U');
+        mouseUp = this.input.keyboard.addKey('t');
+        mouseLeft = this.input.keyboard.addKey('l');
+        mouseDown = this.input.keyboard.addKey('b');
+        mouseRight = this.input.keyboard.addKey('r');
+        powerupActivate = this.input.keyboard.addKey('p');
 
         text1 = this.add.text(10, 10, '');
         text2 = this.add.text(500, 10, '');
@@ -64,70 +93,78 @@ export default class Scene extends Phaser.Scene {
             .withRightCap(this.add.image(0, 0, 'right-cap'))
             .layout()
 
-
-        // this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-
-        // this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-        // this.add.image(400, 300, 'libs');
-
-
-        // const logo = this.add.image(400, 70, 'logo');
-
-        // this.tweens.add({
-        //   targets: logo,
-        //   y: 350,
-        //   duration: 1500,
-        //   ease: 'Sine.inOut',
-        //   yoyo: true,
-        //   repeat: -1
-        // })
-    }
-
-
-    update() {
-
-        //check if the 2 players are ready
-        if (!this.cursors) {
-            return
-        }
-        // if (this.cursors?.space.isDown) {
-        //     console.log('player one ready');
-        //     text2.setText([
-        //         'player one ready'
-        //     ]);
-        // }
-
-        if (this.cursors?.left.isDown) {
-            text3.setText([
-                'player two ready'
-            ]);
-        }
+        //const powerupBar = new PowerupBar(this);
 
         coderona.on('down', () => {
             text2.setText([
                 'player one ready'
             ]);
-        })
+        });
 
         user.on('down', () => {
+            if(userEnabled == true && errorCount == 0) {
+                //we can update counter on user's side
+                console.log('player one clicked')
+            }
             text3.setText([
                 'player two ready'
             ]);
-        })
+        });
 
-        //move user with the coordinates
-        var pointer = this.input.activePointer;
+        mouseUp.on('down', () => {
+            if(mouseCoordinates.y -10 > errorContainer.yStart) {
+                mouseCoordinates.y -= 10;
+            }
+        });
 
-        text1.setText([
-            'x: ' + pointer.worldX,
-            'y: ' + pointer.worldY,
-            'isDown: ' + pointer.isDown
-        ]);
+        mouseDown.on('down', () => {
+            if (mouseCoordinates.y + 10 < errorContainer.yEnd - 60) {
+                mouseCoordinates.y += 10;
+            }
+        });
 
-        this.physics.moveToObject(this.player, pointer, 800);
+        mouseLeft.on('down', () => {
+            if (mouseCoordinates.x - 10 > errorContainer.xStart) {
+                mouseCoordinates.x -= 10;
+            }
+        });
 
+        mouseRight.on('down', () => {
+            if (mouseCoordinates.x + 10 < errorContainer.xEnd - 39) {
+                mouseCoordinates.x += 10;
+            }
+        });
+
+        //listen for powerup, then spawn errors
+        powerupActivate.on('down', () => {
+            console.log('powerup activated, spawn errors');
+
+            userEnabled = false;
+            errorCount += 1;
+
+            let errorX = Phaser.Math.Between(errorContainer.xStart, errorContainer.xEnd - errorWidth);
+            let errorY = Phaser.Math.Between(errorContainer.yStart, errorContainer.yEnd - errorHeight);
+
+            let popup = this.physics.add.sprite(errorX, errorY, 'error-popup').setOrigin(0, 0);
+            let popupX = this.physics.add.sprite(errorX + 200, errorY + 11, 'error-x').setOrigin(0, 0);
+
+            this.physics.add.collider(this.player, popupX, () => {playerHitPopup(popup, popupX)});
+        });        
     }
 
 
+    update() {
+        //check if the 2 players are ready
+        if (!this.cursors) {
+            return
+        }
+
+        if (this.cursors?.left.isDown) {
+            text3.setText([
+                'player two ready'
+            ]);
+        };
+
+        this.physics.moveToObject(this.player, mouseCoordinates, 300, 250);
+    }
 }
--- src/js/main.ts
+++ src/js/main.ts
@@ -8,6 +8,9 @@ const config = {
   height: 1080,
   physics: {
     default: 'arcade',
+    arcade: {
+      debug: true
+    }
   },
   scene: Scene
 };
@@ -15,7 +18,44 @@ const config = {
 const init = () => {
   console.log('Hello from main.ts');
   const game = new Phaser.Game(config);
+  
+  if ("serial" in navigator) {
+    // The Web Serial API is supported.
+    console.log('serial supported');
+    document.querySelector('.serialButton').addEventListener('click', async () => {
+      // Prompt user to select any serial port.
+      const port = await navigator.serial.requestPort();
 
+      //wait for port to open.
+      await port.open({ baudRate: 9600 });
+
+      console.log('port open');
+
+      //start reading for incoming data from the serial device as 
+      // const textDecoder = new TextDecoderStream();
+      // const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+      // const reader = textDecoder.readable.getReader();
+
+
+      // const textEncoder = new TextEncoderStream();
+      // const writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
+
+      // const writer = textEncoder.writable.getWriter();
+      // await writer.write("coderona");
+
+      // // Listen to data coming from the serial device.
+      // while (true) {
+      //   const { value, done } = await reader.read();
+      //   if (done) {
+      //     // Allow the serial port to be closed later.
+      //     reader.releaseLock();
+      //     break;
+      //   }
+      //   // value is a Uint8Array.
+      //   console.log(value);
+      // }
+    });
+  }
 }
 
 init()
